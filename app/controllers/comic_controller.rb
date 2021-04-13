604da06f626f0a9b0e08f0592255a9e0901073a4
GoldenPavilion <rcorystine@gmail.com>
2021-04-13 18:06:49 -0400

    creates comic

--- app/controllers/comic_controller.rb
+++ app/controllers/comic_controller.rb
@@ -4,4 +4,16 @@ class ComicsController < ApplicationController
         erb :'comics/new'
     end
 
+    post '/comics' do
+        @comic = Comic.create(
+            title: params[:title],
+            issue: params[:issue],
+            arc: params[:arc],
+            writer: params[:writer],
+            artist: params[:artist]    
+        )
+        @comic.user_id = current_user.id
+        @comic.save
+    end
+
 end
