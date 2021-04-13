6259fcac1a653da3e4478cbc3e8e4a76261a9c4c
GoldenPavilion <rcorystine@gmail.com>
2021-04-12 20:54:16 -0400

    setup applicationcontroller and user views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,17 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
-    erb :welcome
+    erb :index
+  end
+
+  helpers do
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
   end
 
 end
